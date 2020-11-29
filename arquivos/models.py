f2a4c03a53369844ad1afc22f05e944284385d48
Pilar Baptista Fernandez <pilarfernandez@MacBook-Pro-de-Pilar.local>
2020-11-29 13:15:14 -0300

    .

--- arquivos/models.py
+++ arquivos/models.py
@@ -7,6 +7,6 @@ class Arquivo(models.Model):
     arquivo = models.FileField(validators=[FileExtensionValidator(allowed_extensions=['txt'])])
     
 class Texto(models.Model):
-    conteudo = models.CharField(default="")
+    conteudo = models.CharField(max_length = 100000, default="")
     data =  models.CharField(max_length = 10,verbose_name='Data de criação',default="")
     titulo = models.CharField(max_length=100, help_text = "Entre com o titulo",default="")
